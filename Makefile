e748173dcff7673814d3d05d75654d282a69ee74
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-02-23 22:18:40 +0100

    wip
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: unit-test
+
+unit-test:
+	rm -rf testing
+	cmake -DTARGET_GROUP=test -B testing
+	cd testing
+	make
+	ctest -v .
+	cd ..
+
+
---
+++ amqp_lib/mem_utils.h
@@ -0,0 +1,8 @@
+//
+// Created by Gabriele Santomaggio on 23/02/23.
+//
+
+#ifndef AMQP1_0_MEM_UTILS_H
+#define AMQP1_0_MEM_UTILS_H
+
+#endif //AMQP1_0_MEM_UTILS_H
