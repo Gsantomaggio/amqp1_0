c11285c5fbf4cf96022e49feb90e9ad4a6d708e7
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-02-28 08:43:06 +0100

    Test 700 (#2)
    * Add test
    
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- amqp_lib/amqp_wire_formatting.h
+++ amqp_lib/amqp_wire_formatting.h
@@ -5,14 +5,16 @@
 #ifndef AMQP1_0_AMQP_WIRE_FORMATTING_H
 #define AMQP1_0_AMQP_WIRE_FORMATTING_H
 
+#include <stdlib.h>
 #include <string.h>
 #include <types.h>
+#include <stdint.h>
 
-int read_char(char *source_buffer, char *out_value);
-int read_int(char *source_buffer, int *out_value);
-int read_type(char *source_buffer, char *out_value);
+size_t read_char(char *source_buffer, char *out_value);
+size_t read_int(char *source_buffer, uint32_t *out_value);
+size_t read_type(char *source_buffer, char *out_value);
 
-int read_buffer(char *source_buffer, char *out_value, size_t len);
-int read_application_data(char *source_buffer, PMessage_t message);
+size_t read_buffer(char *source_buffer, char *out_value, size_t len);
+size_t read_application_data(char *source_buffer, PMessage_t message);
 
 #endif //AMQP1_0_AMQP_WIRE_FORMATTING_H
--- test/suite_1/suite_1.c
+++ test/suite_1/suite_1.c
@@ -6,12 +6,35 @@
 #include <unity.h>
 #include <decode.h>
 #include <types.h>
+#include <string.h>
+#include <unistd.h>
 
 
-//void test_New_Message(void) {
-//    PMessage_t msg = newMessage();
-//    TEST_ASSERT_NOT_EMPTY(&msg);
-//}
+int readAmqpBufferFromFile(char *filename, char *buffer, long *len) {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+    }
+
+    FILE *fileptr;
+    strcat(cwd, "/resources/");
+    strcat(cwd, filename);
+    fileptr = fopen(cwd, "rb");  // Open the file in binary mode
+    if (fileptr == NULL) {
+        perror("can't open file");
+        return 0;
+    }
+    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    *len = ftell(fileptr);             // Get the current byte offset in the file
+    rewind(fileptr);                      // Jump back to the beginning of the file
+
+    size_t read = fread(buffer, sizeof(char), *len, fileptr); // Read in the entire file
+    fclose(fileptr); // Close the file
+    return read;
+}
+
 
 void test_DecodeDescribedFormatCode(void) {
     char formatBuff[] = {0x03, 0x11, APPLICATION_DATA};
@@ -24,7 +47,7 @@ void test_DecodeDescribedFormatCode(void) {
 }
 
 void test_MessageParseApplicationDataV8(void) {
-    char formatBuff[] = {0x03, 0x11, APPLICATION_DATA, 0xa0, 0x01, 0x02};
+    char formatBuff[] = {0x03, 0x11, APPLICATION_DATA, FORMAT_CODE_VBIN8, 0x01, 0x02};
     Message_t msg;
     parseAmqp10MessageBuffer(formatBuff, 6, &msg);
     TEST_ASSERT_NOT_EMPTY(msg.data);
@@ -33,15 +56,50 @@ void test_MessageParseApplicationDataV8(void) {
 
 void test_MessageParseApplicationDataV32(void) {
     char formatBuff[] = {0x03, 0x11, APPLICATION_DATA, // format code
-                         0xb0, //FORMAT_CODE_Vbin32
+                         FORMAT_CODE_Vbin32, //FORMAT_CODE_Vbin32
                          0x00, 0x00, 0x00, 0x09, // 9 big endian
                          0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09 // byte buffer
     };
     Message_t msg;
     parseAmqp10MessageBuffer(formatBuff, sizeof(formatBuff), &msg);
     TEST_ASSERT_NOT_EMPTY(msg.data);
-    char expected[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    unsigned char expected[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
     TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, msg.data, 9);
+}
+
+
+void test_MessageParseApplicationDataFromFileBodyV8(void) {
+    char formatBuff[1024];
+    long len;
+    readAmqpBufferFromFile("message_body_this_is_a_amqp_message", formatBuff, &len);
+    Message_t msg;
+    parseAmqp10MessageBuffer(formatBuff, len, &msg);
+    TEST_ASSERT_NOT_EMPTY(msg.data);
+    char expected[] = "this_is_a_amqp_message";
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, msg.data, strlen("this_is_a_amqp_message"));
+}
+
+void test_MessageParseApplicationDataFromFileBody32(void) {
+    char formatBuff[1024];
+    long len;
+    readAmqpBufferFromFile("message_body_700", formatBuff, &len);
+    Message_t msg;
+    parseAmqp10MessageBuffer(formatBuff, len, &msg);
+    TEST_ASSERT_NOT_EMPTY(msg.data);
+    TEST_ASSERT_TRUE(strlen(msg.data) == 700);
+}
+
+
+void test_MessageParseApplicationDataFromFileUnicodeBody32(void) {
+    char formatBuff[1024];
+    long len;
+    readAmqpBufferFromFile("message_body_unicode_500_body", formatBuff, &len);
+    Message_t msg;
+    parseAmqp10MessageBuffer(formatBuff, len, &msg);
+    TEST_ASSERT_NOT_EMPTY(msg.data);
+    char expected[] =
+            "Alan Mathison Turing（1912 年 6 月 23 日 - 1954 年 6 月 7 日）是英国数学家、计算机科学家、逻辑学家、密码分析家、哲学家和理论生物学家。 [6] 图灵在理论计算机科学的发展中具有很大的影响力，用图灵机提供了算法和计算概念的形式化，可以被认为是通用计算机的模型。[7][8][9] 他被广泛认为是理论计算机科学和人工智能之父。 [10]";
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, msg.data, strlen(expected));
 }
 
 
@@ -55,9 +113,11 @@ void tearDown(void) {
 
 int main(void) {
     UNITY_BEGIN();
-//    RUN_TEST(test_New_Message);
     RUN_TEST(test_DecodeDescribedFormatCode);
     RUN_TEST(test_MessageParseApplicationDataV8);
     RUN_TEST(test_MessageParseApplicationDataV32);
+    RUN_TEST(test_MessageParseApplicationDataFromFileBodyV8);
+    RUN_TEST(test_MessageParseApplicationDataFromFileBody32);
+    RUN_TEST(test_MessageParseApplicationDataFromFileUnicodeBody32);
     return UNITY_END();
 }
